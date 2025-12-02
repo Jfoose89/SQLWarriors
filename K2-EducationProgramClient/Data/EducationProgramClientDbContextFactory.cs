6a5104d64c802bc5b6040d87d69782064a7c2531
Adchariya Changtam <adchariya.changtam@chasacademy.se>
2025-12-02 11:44:41 +0100

    Fix for migration and update error: Added context factory

---
+++ K2-EducationProgramClient/Data/EducationProgramClientDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace K2_EducationProgramClient.Data
+{
+    public class EducationProgramClientDbContextFactory
+        : IDesignTimeDbContextFactory<EducationProgramClientDbContext>
+    {
+        public EducationProgramClientDbContext CreateDbContext(string[] args)
+        {
+            // Load configuration (just like Program.cs)
+            var basePath = AppContext.BaseDirectory;
+            basePath = Path.GetFullPath(Path.Combine(basePath, "..", "..", ".."));
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EducationProgramClientDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EducationProgramClientDbContext(optionsBuilder.Options);
+        }
+    }
+}
